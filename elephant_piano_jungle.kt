f5718481b067c1ebd31155900f34eb3eeab61169
tmcdon4 <136752834+tmcdon4@users.noreply.github.com>
2023-06-27 12:28:15 +0700

    Update documentation

---
+++ elephant_piano_jungle.kt
@@ -0,0 +1,41 @@
+package com.hopeforhealthcare
+
+//Awareness about Mental Health
+fun createMentalHealthAwareness() {
+    println("Mental health is an important aspect of overall health. Everyone can benefit from understanding the signs of mental health, steps for promoting mental health, and available resources for support")
+    println("Mental health difficulties may feel overwhelming, but there are many strategies to help manage them. Exercise, mindfulness, positive thinking, and support from family and friends can all help reduce stress and improve mental health.")
+}
+
+//Self-care Tips
+fun selfCareTips() {
+    println("Take time to relax and be kind to yourself - Take a break from work or school, and make time for enjoyable activities")
+    println("Eat a balanced, nutritious diet - Get enough restful and quality sleep, and make sure to drink plenty of fluids") 
+    println("Exercise and move your body - Connect with friends and family, or reach out for support if needed")
+}
+
+//Talk to a Professional
+fun talkToProfessionals(){
+    println("Connecting with a professional can provide guidance for navigating mental health difficulties. A range of professionals is available to support mental health, such as psychologists, therapists, and counselors.")
+    println("Talking to a professional can provide strategies to deal with stress, and can also be helpful for longer-term mental health conditions. Professionals can provide diagnosis, treatment plans, and support for mental health challenges.")
+}
+
+//Know When to Seek Help
+fun whenToSeekHelp(){
+    println("If you are feeling overwhelmed or feeling like it’s too hard to cope with stressful situations, it may be time to seek help. If you or someone you know is exhibiting signs of suicidal thoughts, it is important to seek help right away")
+    println("If you or someone you know is struggling with mental health, there are people that can help. Resources for mental health support such as hotlines, crisis centers, and support groups are available, and can provide assistance in finding a professional.") 
+}
+
+//Raise Awareness and Reduce Stigma
+fun raiseAwareness(){
+    println("Raising awareness about mental health can help reduce stigma. By spreading accurate and up-to-date information about mental health, we can create an environment in which people feel safe to talk about mental health without fear of judgment or stigma.")
+    println("By talking openly about mental health, we can work to reduce the stigma and shame associated with it. By educating others about mental health, we can create a culture in which mental health is respected and supported.")
+}
+
+//Main Function
+fun main(args : Array<String>) {
+    createMentalHealthAwareness()
+    selfCareTips()
+    talkToProfessionals()
+    whenToSeekHelp()
+    raiseAwareness()
+}
