7e89737b2284cc33062809bfb453bfff7d389ae3
tmcdon4 <136752834+tmcdon4@users.noreply.github.com>
2023-07-19 14:09:51 +0700

    Add code comments

---
+++ zebra_ocean_queen.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HopeInHealthcare {
+
+public:
+	
+    void aimToImprove() {
+        
+        cout << "Our aim is to improve healthcare access, quality, and outcomes for all." << endl;
+        
+    }
+    
+    void workToAchieve() {
+        
+        cout << "We work to achieve this goal through better research and data-driven technologies." << endl;
+        
+    }
+    
+    void provideInnovations() {
+        
+        cout << "We provide innovative solutions to enable cost-effective healthcare access, quality, and outcomes." << endl;
+        
+    }
+
+};
+
+int main() {
+
+    HopeInHealthcare high;
+    
+    high.aimToImprove();
+    high.workToAchieve();
+    high.provideInnovations();
+    
+    return 0;
+    
+}
